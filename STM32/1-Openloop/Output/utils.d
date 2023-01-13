e208e197334f3ea72c4f50477c671d49bcb2fcb1
LAPTOP-K9F5A5PL\GYM <ganyong.mo@autonoma.cat>
2023-01-13 17:23:30 +0800

    STM32

---
+++ STM32/1-Openloop/Output/utils.d
@@ -0,0 +1,3 @@
+..\output\utils.o: ..\Simple_FOC\utils.c
+..\output\utils.o: ..\Simple_FOC\utils.h
+..\output\utils.o: G:\2-Installed\Keil5\ARM\ARMCC\Bin\..\include\math.h
---
+++ STM32/1-Openloop/User/timer.h
@@ -0,0 +1,20 @@
+
+#ifndef TIMER_H
+#define TIMER_H
+
+#include "stm32f10x.h"
+
+
+#define PWM_Period  1440
+#define MotorEnable GPIO_SetBits(GPIOA, GPIO_Pin_3)
+#define MotorDisable GPIO_ResetBits(GPIOA, GPIO_Pin_3)
+
+
+void TIM2_PWM(void);
+void TIM3_1ms(void);
+void PWM_Enable(void);
+
+
+#endif
+
+
